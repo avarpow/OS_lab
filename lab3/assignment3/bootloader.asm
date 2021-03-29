ab9bc9e106b1891747a4db2bf7ff357ba47e7b47
avarpow <648120201@qq.com>
2021-03-29 15:25:37 -0700

    lab3 assignment3 ok

--- lab3/assignment3/bootloader.asm
+++ lab3/assignment3/bootloader.asm
@@ -63,24 +63,206 @@ mov ss, eax
 mov eax, VIDEO_SELECTOR
 mov gs, eax
 
-mov ecx, protect_mode_tag_end - protect_mode_tag
-mov ebx, 80 * 2
-mov esi, protect_mode_tag
-mov ah, 0x3
-output_protect_mode_tag:
-    mov al, [esi]
-    mov word[gs:ebx], ax
-    add ebx, 2
-    inc esi
-    loop output_protect_mode_tag
+; 初始化栈指针
+mov ah, 0x0 ;
+mov al, '0'
+;初始化bp
+mov bp,0
+;初始化位置
+mov bh,9 ;字符1初始行
+mov bl,0 ;字符1初始列
+mov ch,16 ;字符2初始行
+mov cl,79 ;字符2初始列
+mov dh,0 ;字符1初始方向
+mov dl,3 ;字符2初始方向
+call clear_screen
+print:
+    pushad
+    mov dh,0
+    mov dl,bh
+    imul dx,80
+    mov bh,0
+    add dx,bx
+    imul dx,2
+    mov bp,dx
+    mov [gs:bp],ax
+    popad
+    call print_id
+    call pause
+    call change_text
 
+    pushad
+    mov dh,0
+    mov dl,ch
+    imul dx,80
+    mov ch,0
+    add dx,cx
+    imul dx,2
+    mov bp,dx
+    mov [gs:bp],ax
+    popad
+    call print_id
+    call pause
+    call change_text
+    call change_b_position
+    call change_c_position
+    jmp print
+
+change_text: ;改变颜色和数字
+    inc ah
+    inc al
+    cmp al,':'
+    jge set_al_zero
+    jmp change_text_end
+set_al_zero: ;超过9重新设置为0
+    mov al,'0'
+change_text_end:
+    ret
+
+change_b_position:
+    cmp dh,0
+    je right_down_b
+    cmp dh,1
+    je right_up_b
+    cmp dh,2
+    je left_down_b
+    cmp dh,3
+    je left_up_b
+    right_down_b:
+        inc bh
+        inc bl
+        jmp check_bl
+    right_up_b:
+        dec bh
+        inc bl
+        jmp check_bl
+    left_down_b:
+        inc bh
+        dec bl
+        jmp check_bl
+    left_up_b:
+        dec bh
+        dec bl
+        jmp check_bl
+check_bl:
+    cmp bl,0
+    jle change_left_right
+    cmp bl,79
+    jge change_left_right
+    jmp check_bh
+change_left_right: ;改变方向左右
+    xor dh,0x02
+check_bh:
+    cmp bh,0
+    jle change_up_down
+    cmp bh,24
+    jge change_up_down
+    jmp change_end
+change_up_down:
+    xor dh,0x01  ;改变方向上下
+change_end:
+    ret
+
+change_c_position:
+    cmp dl,0
+    je right_down_c
+    cmp dl,1
+    je right_up_c
+    cmp dl,2
+    je left_down_c
+    cmp dl,3
+    je left_up_c
+    right_down_c:
+        inc ch
+        inc cl
+        jmp c_check_cl
+    right_up_c:
+        dec ch
+        inc cl
+        jmp c_check_cl
+    left_down_c:
+        inc ch
+        dec cl
+        jmp c_check_cl
+    left_up_c:
+        dec ch
+        dec cl
+        jmp c_check_cl
+c_check_cl:
+    cmp cl,0
+    jle c_change_left_right
+    cmp cl,79
+    jge c_change_left_right
+    jmp c_check_ch
+c_change_left_right:
+    xor dl,0x02 ;改变方向左右
+c_check_ch:
+    cmp ch,0
+    jle c_change_up_down
+    cmp ch,24
+    jge c_change_up_down
+    jmp c_change_end
+c_change_up_down:
+    xor dl,0x01  ;改变方向上下
+c_change_end:
+    ret
+
+pause: ;延时函数
+    pushad
+    mov ecx,delay
+    in_loop:
+        call pause_2
+        loop in_loop
+    in_end:
+    popad
+    ret
+
+pause_2:
+    pushad
+    mov ecx,delay2
+    in_loop_2:
+        loop in_loop_2
+    in_end_2:
+    popad
+    ret
+
+clear_screen:
+    pushad
+    mov ax,0
+    mov ecx,2000
+    mov di,0
+    clc_loop:
+        mov word [gs:di],ax
+        add di,2
+        loop clc_loop
+    popad
+    ret
+
+print_id:
+    pushad
+    mov si, stuid          ; 显示姓名和学号
+    mov di, 64
+    mov cx, 16
+print_char:
+    mov ah,0x47
+    mov al, [si]
+    inc si
+    mov word [gs:di],ax
+    add di,2
+    loop print_char
+    popad
+    ret
 jmp $ ; 死循环
 
 pgdt dw 0
      dd GDT_START_ADDRESS
+delay equ 5000
+delay2 equ 10
+stuid db 'Avarpow 18324034'
 
 bootloader_tag db 'run bootloader'
 bootloader_tag_end:
 
 protect_mode_tag db 'enter protect mode'
 protect_mode_tag_end:
+
