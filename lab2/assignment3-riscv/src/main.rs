e3dad0156e7d872f692d66d8cca318e467ba3b10
avarpow <648120201@qq.com>
2021-03-29 11:13:03 -0700

    lab3 assignment1.1

---
+++ lab2/assignment3-riscv/src/main.rs
@@ -0,0 +1,19 @@
+
+#[no_mangle]
+static mut a1:int=14;
+#[no_mangle]
+static mut a2:int=16;
+#[no_mangle]
+static mut if_flag:int=0;
+
+#[no_mangle]
+static mut while_flag: (mut *char), random_buffer:(mut *char);
+
+fn main() {
+    println!(">>> begin test");
+    if a2>=12 {
+        while_flag 
+    }
+    
+
+}
