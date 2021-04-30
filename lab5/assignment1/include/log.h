cdf68848ae382d336c62a4874acbc209313f9499
avarpow <648120201@qq.com>
2021-04-30 18:43:33 +0800

    lab5 assignment1 ok

--- lab5/assignment1/include/log.h
+++ lab5/assignment1/include/log.h
@@ -1,17 +1,13 @@
 #ifndef LOG_H
 #define LOG_H
 
-#define OFF 0
 #define DEBUG 5
 #define INFO 10
 #define ERROR 15
 
-
-
 int printf_debug(const char *const fmt, ...);
 int printf_error(const char *const fmt, ...);
 int printf_info(const char *const fmt, ...);
-
 
 #endif
 
--- lab5/assignment1/src/kernel/setup.cpp
+++ lab5/assignment1/src/kernel/setup.cpp
@@ -16,11 +16,14 @@ extern "C" void setup_kernel()
     stdio.initialize();
     interruptManager.enableTimeInterrupt();
     interruptManager.setTimeInterrupt((void *)asm_time_interrupt_handler);
-    printf("print percentage: %%\n"
-           "print char \"N\": %c\n"
-           "print string \"Hello World!\": %s\n"
-           "print decimal: \"-1234\": %d\n"
-           "print hexadecimal \"0x7abcdef0\": %x\n",
-           'N', "Hello World!", -1234, 0x7abcdef0);
+    printf_error("error message :%d %x\n", -1234, 0x7abcdef0);
+    printf_debug("debug message :%d %x\n", -1234, 0x7abcdef0);
+    printf_info ("info message :%d %x\n", -1234, 0x7abcdef0);
     asm_halt();
 }
+    // printf("print percentage: %%\n"
+    //        "print char \"N\": %c\n"
+    //        "print string \"Hello World!\": %s\n"
+    //        "print decimal: \"-1234\": %d\n"
+    //        "print hexadecimal \"0x7abcdef0\": %x\n",
+    //        'N', "Hello World!", -1234, 0x7abcdef0);
