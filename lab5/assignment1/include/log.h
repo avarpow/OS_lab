e3ac4b570242bd337a312019f4d9783dd28db19c
avarpow <648120201@qq.com>
2021-04-30 17:36:49 +0800

    lab5 assignment1

---
+++ lab5/assignment1/include/log.h
@@ -0,0 +1,17 @@
+#ifndef LOG_H
+#define LOG_H
+
+#define OFF 0
+#define DEBUG 5
+#define INFO 10
+#define ERROR 15
+
+
+
+int printf_debug(const char *const fmt, ...);
+int printf_error(const char *const fmt, ...);
+int printf_info(const char *const fmt, ...);
+
+
+#endif
+
